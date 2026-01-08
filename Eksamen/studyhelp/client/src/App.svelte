2d3458119991aabe9bfbd5f3b2779ae4a3831239
masj0002 <masj0002@stud.kea.dk>
2026-01-08 13:35:08 +0100

    Added is authintecated so you can only see login features when not logged in and you can only see logged in features when logged in

--- Eksamen/studyhelp/client/src/App.svelte
+++ Eksamen/studyhelp/client/src/App.svelte
@@ -1,9 +1,21 @@
 <script>
+  import { onMount } from 'svelte'
   import { Router, Link, Route } from 'svelte-routing'
   import 'toastr/build/toastr.min.css'
   import Login from './pages/Login.svelte'
   import Profile from './pages/Profile.svelte'
   import StudyRoom from './pages/StudyRoom.svelte'
+  import { user, isAuthenticated } from './store/userStore'
+  import { fetchGet } from '../util/fetchUtil.js'
+
+  onMount(async () => {
+    const response = await fetchGet('/users/id')
+    if (response?.status === 200) {
+      user.set(response.data)
+    } else {
+      user.set({ username: '', email: '', role: '' })
+    }
+  })
 </script>
 
 <Router>
@@ -13,9 +25,13 @@
     </div>
     <nav class="nav-links">
       <Link to='/'>Home</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/profile'>Profile</Link>
-      <Link to='/study-room'>Study Room</Link>
+      {#if !$isAuthenticated}
+        <Link to='/login'>Login</Link>
+      {/if}
+      {#if $isAuthenticated}
+        <Link to='/profile'>Profile</Link>
+        <Link to='/study-room'>Study Room</Link>
+      {/if}
     </nav>
   </header>
   <main class="page-content">
@@ -25,8 +41,13 @@
         <h1>Study Helper</h1>
         <p>Organize your revision, share tips in real time, and keep your focus sharp.</p>
         <div class="hero-actions">
-          <Link to='/login' class="primary">Login or Signup</Link>
-          <Link to='/study-room' class="secondary">Visit Study Room</Link>
+          {#if $isAuthenticated}
+            <Link to='/study-room' class="primary">Enter Study Room</Link>
+            <Link to='/profile' class="secondary">View Profile</Link>
+          {:else}
+            <Link to='/login' class="primary">Login or Signup</Link>
+            <Link to='/login' class="secondary">Need an account?</Link>
+          {/if}
         </div>
       </section>
     </Route>
